224793942d2e5bb6b48bde20cfe774f03a68b222
Arpan-Das <arpandas200015@outlook.com>
2021-09-16 20:26:42 +0530

    this is a test commet

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <h2>Duare Doctor</h2>
+      dfldsjflsdjg
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore labore aperiam cumque nam, culpa iure est!
+       Natus, nobis! Esse blanditiis magni recusandae ipsum in debitis fugit hic, dolorum mollitia! Maiores, eligendi,
+        expedita repellendus, eum excepturi eius nulla corporis debitis nemo iusto aliquam aliquid deleniti illo rerum?
+         Expedita asperiores pariatur saepe ut quia deleniti dolorem, numquam in esse. Molestiae pariatur, ullam cumque
+          nulla inventore sunt excepturi ratione. Minus accusantium quam laboriosam voluptates a ut quas. Fugiat porro 
+          aspernatur, iure quos expedita magni sit dignissimos debitis unde voluptatibus saepe, velit omnis enim error! 
+          Fugit cumque aliquam, ipsum, odit eligendi fuga, deleniti magnam perspiciatis aliquid molestiae corrupti 
+          ducimus aperiam optio est officia! Similique voluptatem non nulla consequatur maxime vel, nam odit quidem 
+          recusandae? Inventore, quisquam dolorum.
     </div>
   );
 }
